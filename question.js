11c344b632efb2faf5c8108a1ffb44c1044dbd17
LearnAndExplore <pangpang_1993_0506@yahoo.com.hk>
2017-11-24 15:24:30 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,18 @@
 'use strict';
+class Question{
+  constructor(content){
+    this.content = content;
+    this.save();
+  }
+  static All(){
+    return this._All;
+  }
+  save(){
+    this.constructor._All.push(this);
+
+  }
+  static Find(ID){
+    return this._All[ID-1];
+  }
+}
+Question._All = []
